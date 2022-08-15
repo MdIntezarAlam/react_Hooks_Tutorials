e2172059d92a1c36ca4dbb9f2caa7d2fe63f1ee4
mdintezaralam <mintezar99@gmail.com>
2022-08-16 00:23:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Toogle from "./components/UseStates/Toogle";
+// import Usestatewith_Array from "./components/UseStates/Usestatewith_Array";
+// import Usestate_withObject from "./components/UseStates/Usestate_withObject";
+// import Add_delete from "./components/Add_delete";
+// import "./App.css";
+// import Plcaeholder_Api from "./components/UseEffects/plceholder/Plcaeholder_Api";
+// import UseEffect_One from "./components/UseEffects/UseEffect_One";
+// import UseEffct_CleanUP_Fun from "./components/UseEffects/UseEffct_CleanUP_Fun";
+// import Useeffect_API from "./components/UseEffects/Useeffect_API";
+import UseReducere from "./components/useReducere/UseReducere";
+// import UseRef from "./components/useRef_Uncntrolled_Components/UseRef";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1 style={{ textAlign: "center", color: "black" }}>
+        This is Toggle Components
+      </h1>
+      <Toogle />
+      <hr />
+
+      <h1 style={{ textAlign: "center", color: "black" }}>
+        This is Usestatewith_Array Components
+      </h1>
+      <Usestatewith_Array />
+      <hr />
+      <h1 style={{ textAlign: "center", color: "black" }}>
+        This is Usestate_withObject Components
+      </h1>
+      <Usestate_withObject />
+      <hr />
+      <Add_delete /> */}
+      {/* <UseEffect_One /> */}
+      {/* <UseEffct_CleanUP_Fun /> */}
+      {/* <Useeffect_API /> */}
+
+      {/* useEfect with placeholder Api*/}
+      {/* <Plcaeholder_Api /> */}
+      {/* UseRef */}
+      {/* <UseRef /> */}
+      {/* UseReducere */}
+      <UseReducere />
+
     </div>
   );
-}
+};
 
 export default App;
