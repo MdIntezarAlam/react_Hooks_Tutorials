2003a4816935f90de667c68bd60438078b7bcb0e
mdintezaralam <mintezar99@gmail.com>
2022-08-16 11:44:49 +0530

    second

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import React from "react";
 // import UseEffect_One from "./components/UseEffects/UseEffect_One";
 // import UseEffct_CleanUP_Fun from "./components/UseEffects/UseEffct_CleanUP_Fun";
 // import Useeffect_API from "./components/UseEffects/Useeffect_API";
-import UseReducere from "./components/useReducere/UseReducere";
+// import UseReducere from "./components/useReducere/UseReducere";
 // import UseRef from "./components/useRef_Uncntrolled_Components/UseRef";
+import One from "./components/useContextHooks/first/One";
+import SecMainComponent from "./components/useContextHooks/second/SecMainComponent";
 
 const App = () => {
   return (
@@ -40,8 +42,11 @@ const App = () => {
       {/* UseRef */}
       {/* <UseRef /> */}
       {/* UseReducere */}
-      <UseReducere />
+      {/* <UseReducere /> */}
+      {/* useContext Hooks */}
+      {/* <One  /> */}
 
+      <SecMainComponent /> 
     </div>
   );
 };
--- src/components/useReducere/UseReducere.js
+++ src/components/useReducere/UseReducere.js
@@ -4,6 +4,7 @@
 //step2=> define initial state at the top of the main fun or other compo
 //step3=> define reducre as a function which takes two arguments at the top of the main function
 import React, { useReducer } from "react";
+import Two from "./Two";
 const initialState = 0; //step2=>
 
 //step3=> and it will return
@@ -25,6 +26,9 @@ const One = () => {
       <h2>State {state}</h2>
       <button onClick={() => dispatch({ type: "INCRE" })}>Incerement:</button>
       <button onClick={() => dispatch({ type: "DECRE" })}>Decerement:</button>
+      <hr />
+      <br />
+      <Two />
     </div>
   );
 };
