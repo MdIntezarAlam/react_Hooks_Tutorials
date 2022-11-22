56ebee857157d8f947acdc4d1abfa50c1e6b47de
Md Intezar Alam <93022029+MdIntezarAlam@users.noreply.github.com>
2022-11-22 15:19:41 +0530

    Update One.js

--- src/components/useContextHooks/first/One.js
+++ src/components/useContextHooks/first/One.js
@@ -1,6 +1,9 @@
 //useContext
-//it provides a way to pass data or state through the component tree without having to pass props down manually through each nested component.
-//It is designed to share data that can be considered as global data for a tree of React components, such as the current authenticated user or theme(e.g. color, paddings, margins, font-sizes)
+//it provides a way to pass data or state through the component tree..
+// without having to pass props down manually through each nested component.
+//It is designed to share data that can be considered as ..
+// global data for a tree of React components, such..
+// as the current authenticated user or theme(e.g. color, paddings, margins, font-sizes)
 //syntax =>const createContext = useContext(initialValue);
 //step1=>  createContext at the top of the function or other components and export it
 //step2=>  createContext aur .provider ko wrap kar ke likhe aur usme value pass kre whatever we want
